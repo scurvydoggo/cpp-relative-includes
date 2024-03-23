dfb4de31af97256b85c68a43ca44e93e4ed32ca4
Andreas Nygard <andreas.nygard@gmail.com>
2024-03-24 00:52:50 +1100

    Identify includes needing rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::collections::HashSet;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
 use std::path::Path;
 use std::path::PathBuf;
 
+use anyhow::Context;
 use anyhow::Result;
 use clap::Parser;
+use regex::Regex;
 use walkdir::WalkDir;
 
 #[derive(Parser)]
@@ -18,29 +23,48 @@ fn main() -> Result<()> {
 
     println!("Project path: {:?}", args.project_path);
 
-    let header_files: HashSet<PathBuf> = files_with_ext(root, &[".h"]);
-    let source_files: HashSet<PathBuf> = files_with_ext(root, &[".h", ".cpp"]);
+    let header_files: HashSet<String> = files_with_ext(root, &[".h"], true);
+    let source_files: HashSet<String> = files_with_ext(root, &[".h", ".cpp"], false);
 
-    for source_file in source_files {
+    for source_path in source_files {
         // Read the file
+        let file = File::open(source_path)?;
+        let lines = BufReader::new(file).lines();
 
-        // Parse each #include
+        for line in lines {
+            let l = line?;
 
-        // See if it matches a header file
+            // See if it matches an include
+            let re = Regex::new(r#"#include "(?<file>.*?)""#)?;
+            if let Some(matches) = re.captures(l.as_str()) {
+                let include = matches.name("file").expect("not possible").as_str();
 
-        // Rewrite it as relative
+                if (header_files.contains(include)) {
+                    // Rewrite it as relative
+                    println!("Needs rewrite: {}", include);
+                }
+            }
+        }
     }
 
     Ok(())
 }
 
-fn files_with_ext(project_path: &Path, exts: &[&str]) -> HashSet<PathBuf> {
+fn files_with_ext(project_path: &Path, exts: &[&str], remove_prefix: bool) -> HashSet<String> {
+    let prefix_len = project_path.to_string_lossy().len() + 1; // +1 to compensate for leading \
     WalkDir::new(project_path).into_iter()
         .filter_map(|f| f.ok())
         .filter(|f| {
             let name = f.file_name().to_string_lossy();
             exts.into_iter().any(|&ext| name.ends_with(ext))
         })
-        .map(|f| f.into_path())
+        .map(|f| f.into_path().to_string_lossy().to_string())
+        .map(|f| {
+            if remove_prefix {
+                f.get(prefix_len..).expect("not possible").to_string()
+            } else {
+                f
+            }
+        })
         .collect()
 }
